660c6a02c17fbc9d095147e07ea1cbed2bfc9e6c
LACOUTURE Ludovic <email@lacoludo.com>
2017-05-18 18:09:01 +0200

    static app

---
+++ js/tiota.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+})
